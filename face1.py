7c78d63f58af31656118300d7b4f5795e1b78dd9
pamelafiguer <pamelaestafani2005@gmail.com>
2025-05-28 21:31:53 -0500

    face

--- face1.py
+++ face1.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 faceClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-image = cv2.imread('oficina.png')
+image = cv2.imread('oficina2.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 face = faceClassif.detectMultiScale(gray,
                                    scaleFactor=1.1,
-                                   minNeighbors=5,
-                                   minSize=(30, 30),
-                                   maxSize=(200, 200))
+                                   minNeighbors=4,
+                                   minSize=(20, 20),
+                                   maxSize=(100, 100))
 for (x, y, w, h) in face:
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow('image', image)
